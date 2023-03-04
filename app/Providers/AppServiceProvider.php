2cc7fb8b9c6f2ab130790a2dfbb9cdf523022c7b
ZED <zedequis@gmail.com>
2023-03-03 22:58:38 -0400

    http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if( ( $app_env != 'local' ) && ( $app_env != 'testing' ) && $app_env !="" ){
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
